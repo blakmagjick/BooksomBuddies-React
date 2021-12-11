27e453c4ac664e9f54d45ef3e2bbfc3307ae92d2
Suzy Q <blakmagjick@users.noreply.github.com>
2021-12-11 08:50:45 -0800

    Tailwind updates for deployment

--- src/books/Profile.js
+++ src/books/Profile.js
@@ -10,15 +10,17 @@ export default class Profile extends Component {
     render () {
         return (
             <React.Fragment>
+            <p className='text-2xl font-semibold'>Profiles</p>
                 {this.props.profiles.map((profile, i) => {
                     return (
                         <>
-                            <p>Profile</p>
+                        <div className='profiles'>
                             <span>Username: {profile.username.username}</span><br />
                             <span>Name: {profile.name}</span><br />
                             <span>Location: {profile.location}</span><br />
                             <span>Fave Book: {profile.favebook}</span><br />
                             <span>Wish List: {profile.wishlist}</span><br />
+                        </div>
                         </>
                     )
                 })}
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,5 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
 }
