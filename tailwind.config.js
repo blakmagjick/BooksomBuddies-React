1701a18bafe1e513ab73266da0572fd41caa10ad
Suzy Q <blakmagjick@users.noreply.github.com>
2021-12-11 09:22:23 -0800

    Added /src files to tailwind config for deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,5 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,jsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
 }
